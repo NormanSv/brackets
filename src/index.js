09525527d31a8fbd7452c71e126b2c69a89bcd3c
NormanSv <game260@mail.ru>
2020-10-24 11:21:27 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let MapBrackets = new Map(bracketsConfig),
+   stack = [];
+   for (let i = 0; i< str.length; i++)
+   {
+     str[i] === MapBrackets.get(stack[stack.length-1]) ? stack.pop() : stack.push(str[i]);
+   }
+   return stack.length === 0;
 }
